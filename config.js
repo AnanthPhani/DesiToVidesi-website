fbf9830e6fcda83248bec34b4933897cb9a3e1f1
AnanthPhani <58096933+AnanthPhani@users.noreply.github.com>
2026-07-14 11:54:49 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,15 @@
+window.APP_CONFIG = {
+  contactEmail: "",
+  whatsappNumber: "919110342317",
+  phoneDisplay: "+91 91103 42317",
+  websiteUrl: "https://www.desitovidesi.com",
+  websiteDisplay: "www.desitovidesi.com",
+  instagramUrl: "https://instagram.com/desitovidesi",
+  instagramDisplay: "@desitovidesi",
+  businessAddress: [
+    "H.no 1079, NRSA Colony",
+    "Bhagat Singh Nagar PH-2",
+    "Kukatpally, Hyderabad - 500084"
+  ],
+  formspreeEndpoint: "https://formspree.io/f/YOUR_FORM_ID"
+};
